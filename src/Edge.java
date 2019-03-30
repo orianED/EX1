86b11df6f3456480b84a2df07d363541f76dda49
Orian Edri <orian.derei@gmail.com>
2019-03-30 19:48:00 +0300

    vertex and edge

---
+++ src/Edge.java
@@ -0,0 +1,8 @@
+public class Edge {
+    private Vertex v1, v2;
+
+    public Edge(Vertex v1, Vertex v2) {
+        this.v1 = v1;
+        this.v2 = v2;
+    }
+}
---
+++ src/Vertex.java
@@ -0,0 +1,33 @@
+public class Vertex {
+    private double x, y, z;
+
+    public Vertex(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
+}
